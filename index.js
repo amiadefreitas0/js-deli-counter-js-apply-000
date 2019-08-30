2056e70956d5aed14dd9415c6e17a3a1149580e6
Amia Defreitas <amia.defreitas0@gmail.com>
2019-08-30 19:10:15 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,54 @@
+//function current line
+//parameter is line
+//length is the number of elements in your array
+//console log allows you to put it in the console
+var customers = [];
+//create a current line function using if and for ; create a variable
+function currentLine(line) {
+  if(!line.length) {
+    return "The line is currently empty.";
+  }
+  //i used .push so that it can add new items at the end of an array and returns
+  var linename = [];
+  for (var i = 0; i < line.length; i++) {
+    linename.push(i + 1 + " " + line[i]);
+  }
+  console.log("The line is currently: " +  linename);
+  return "The line is currently: "  +linename.join(", ");
+}
+//nowServing function
+// ! mean not ="removed .length"
+function nowServing(serving) {
+  if (!serving.length) {
+    console.log("There is nobody waiting to be served!");
+  } else {
+    console.log("Currently serving " + serving.shift());
+  }
+}
+//take a number function
+///line is number of people
+function takeANumber(numberofpeople, name) {
+  numberofpeople.push(name);
+ console.log(
+    "Welcome, " +
+      name +
+      ". You are number " +
+      numberofpeople.length +
+      " in line."
+  );
+
+}
+
+takeANumber(customers, "Ada");
+takeANumber(customers, "Grace");
+takeANumber(customers, "Matt");
+takeANumber(customers, "Kent");
+nowServing(customers);
+currentLine(customers);
+nowServing(customers);
+currentLine(customers);
+nowServing(customers);
+currentLine(customers);
+nowServing(customers);
+currentLine(customers);
+nowServing(customers);
